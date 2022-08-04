61f6384b010da6164814c656934c09ff19724b4b
YK2022-July <105860045+YK2022-July@users.noreply.github.com>
2022-08-04 12:25:04 +0000

    [Finish]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,7 +11,11 @@ class ListsController < ApplicationController
     #3. データをデータベースに保存するためのsaveメソッド実行
     list.save
     #4. トップ画面へリダイレクト
-    redirect_to '/top'
+    #redirect_to '/top'
+
+    #redirect_to '/top'をコメントアウトして、以下コードに変更
+    #詳細画面へリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -19,6 +23,7 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
